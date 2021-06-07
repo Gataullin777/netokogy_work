9a3f26efde6d2189da953f1969abd29e9fb6fb05
Radif <rudolf18111992@gmail.com>
2021-06-07 18:02:51 +0300

    task select, insert, delete

---
+++ home_work_data_base/SELECT_requests.py
@@ -0,0 +1,47 @@
+import sqlalchemy
+from pprint import pprint
+
+
+#db =
+
+engine = sqlalchemy.create_engine(db)
+
+connection = engine.connect()
+
+
+#1)
+# result = connection.execute(''' SELECT name, year_of_issue FROM album
+#                                     WHERE year_of_issue = 2018;''').fetchall()
+# print(result)
+
+#2
+# result = connection.execute(''' SELECT name, duration FROM track
+# ORDER BY duration DESC
+# LIMIT 1
+# ;''').fetchall()
+
+#pprint(result)
+
+#3
+# result = connection.execute(''' SELECT name, duration FROM track
+# WHERE duration >= 3.5 ;''').fetchall()
+#
+# pprint(result)
+
+#4
+# result = connection.execute(''' SELECT name, year_of_issue FROM collection
+# WHERE year_of_issue BETWEEN 2018 AND 2020 ;''').fetchall()
+#
+# pprint(result)
+
+#5
+# result = connection.execute(''' SELECT name_or_pseudonym FROM performers
+# WHERE name_or_pseudonym NOT LIKE  '%% %%';''').fetchall()
+#
+# pprint(result)
+
+#6
+# result = connection.execute('''SELECT name FROM track
+# WHERE name ILIKE '%%my%%' ;''').fetchall()
+#
+# print(result)
